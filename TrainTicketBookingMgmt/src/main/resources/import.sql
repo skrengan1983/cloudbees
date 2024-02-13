b82629e55b3be8c234a3cdb4ad9a62dff32969ff
skrengan1983 <skrengan1983@gmail.com>
2024-02-13 21:44:10 +0530

    changed data.sql to import.sql file

---
+++ TrainTicketBookingMgmt/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into train_ticket_booking(id, t_user,t_from, t_to, price_paid) values (1, 'kasthuri', 'london', 'france', 500.75);
+insert into train_ticket_booking(id, t_user,t_from, t_to, price_paid) values (2, 'rengan',  'france', 'london',500.75);
+insert into train_user(id, first_name,last_name, email_address) values (1, 'kasthuri', 'r', 'skrengan19831@gmail.com');
+insert into train_user(id, first_name,last_name, email_address) values (2, 'rengan', 'k', 'skrengan19832@gmail.com');
